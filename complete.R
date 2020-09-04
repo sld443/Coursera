0ec100487594e98d01d45d412afad76fd63fd42b
sld443 <lhwsw1030@gmail.com>
2020-09-04 17:27:29 +0900

    For assignment and more explanation

--- complete.R
+++ complete.R
@@ -1,12 +1,22 @@
 complete <- function(directory, id = 1:332){
   results <- data.frame(id=numeric(0), nobs=numeric(0))
+  # before operating for loops, make a place first.
+  
   for(monitor in id){
     path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+    # it can help define the path of the specdata folder.
+    # sprintf() converts the parameters to string with a special rule.
+    # %03d(Format Symbol) means the signed decimal integer with 3 digits.
     monitor_data <- read.csv(path)
+    # according to the path defined above, read files and assign to a variable, monitor_data.
     interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
     interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+    # in each step, the NA value of the two variable would be treated.
+    # as a result, interested_data consists of the valuable which doesn't include any NA values.
     nobs <- nrow(interested_data)
+    # calculate the length of ROW.
     results <- rbind(results, data.frame(id=monitor, nobs=nobs))
+    # with these codes, We can know what the number of complete cases is.
   }
   results
 }
--- corr.R
+++ corr.R
@@ -1,9 +1,13 @@
 corr <- function(directory, threshold = 0){
 
   cor_results <- numeric(0)
+  # before operating for loops, make a place first.
   
   complete_cases <- complete(directory)
+  # the function complete above has already defined before.
   complete_cases <- complete_cases[complete_cases$nobs>=threshold, ]
+  # in this code, complete_cases is not a function but an object.
+  # the data consists of the number of complete data which is greater than threshold mentioned above.
 
   if(nrow(complete_cases)>0){
     for(monitor in complete_cases$id){
@@ -13,9 +17,12 @@ corr <- function(directory, threshold = 0){
 
       interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
       interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+      # the above codes are same as complete function.
       sulfate_data <- interested_data["sulfate"]
       nitrate_data <- interested_data["nitrate"]
+      # assign the value named "sulfate" and "nitrate" to each object.
       cor_results <- c(cor_results, cor(sulfate_data, nitrate_data))
+      # calculate "Pearson Correlation Coefficient" between the two valuables.
     }
   }
   cor_results
